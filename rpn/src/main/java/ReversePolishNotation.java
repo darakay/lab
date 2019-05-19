fa25c8b757918ea236c3ab46a139d11e139b396e
Daria <daria.kaygo@yandex.ru>
2019-05-19 16:09:07 +0500

    Add Task 2

--- rpn/src/main/java/ReversePolishNotation.java
+++ rpn/src/main/java/ReversePolishNotation.java
@@ -1,6 +1,49 @@
-public class ReversePolishNotation {
+import java.util.HashMap;
+import java.util.Stack;
+import java.util.function.BinaryOperator;
 
-    public int calculate(String calculatedValue){
-        return 0;
+public class ReversePolishNotation {
+    private static HashMap<String, BinaryOperator<Double>> operations = new HashMap<>();
+    private static Stack<Double> valueStack = new Stack<>();
+
+    static {
+        operations.put("+", (a, b) -> a+b);
+        operations.put("-", (a, b) -> b-a);
+        operations.put("*", (a, b) -> a*b);
+        operations.put("/", (a, b) -> b/a);
+        operations.put("^", (a, b) -> Math.pow(b, a));
     }
+
+    public static double calculate(String calculatedValue) throws CalculateException {
+        if(calculatedValue.isEmpty())
+            return 0;
+        for(String c : calculatedValue.split(" "))
+            if(c.matches("[0-9]+"))
+                valueStack.push(parse(c));
+            else if(operations.containsKey(c))
+                handleOperation(operations.get(c));
+            else
+                throw new CalculateException("Unexpected symbol "+c+".");
+        return getResult();
+    }
+
+    private static void handleOperation(BinaryOperator<Double> bio) throws CalculateException {
+        if(valueStack.size() < 2)
+            throw new CalculateException("Invalid calculate value.");
+        double firstValue = valueStack.pop();
+        double secondValue = valueStack.pop();
+        valueStack.push(bio.apply(firstValue, secondValue));
+    }
+
+    private static double getResult() throws CalculateException {
+        if(valueStack.size() > 1)
+            throw new CalculateException("Invalid calculate value.");
+        return valueStack.pop();
+    }
+
+    private static double parse(String value){
+        return Double.parseDouble(value);
+    }
+
+
 }
--- rpn/src/test/java/ReversePolishNotationTest.java
+++ rpn/src/test/java/ReversePolishNotationTest.java
@@ -1,12 +1,42 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReversePolishNotationTest {
 
     @ParameterizedTest
+    @CsvSource({"'8 9 ?', '?'", "'9 0 = ', '='", "'4 2 %', '%'", "'8 : 9', ':'"})
+    void calculate_ShouldThrowCalculateException_ThenValueContainsInvalidSymbols
+            (final String calculatedValue, String unexpectedSymbol) {
 
-    void calculate() {
+        Throwable exception = assertThrows(CalculateException.class,
+                () -> { ReversePolishNotation.calculate(calculatedValue);});
+
+        assertThat(exception).hasMessage("Unexpected symbol "+unexpectedSymbol+".");
+    }
+
+    @Test
+    void calculate_ShouldReturnZero_ThenValueIsEmpty() throws CalculateException {
+        assertThat(ReversePolishNotation.calculate("")).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'2 3 ^ 2 /', 4", "'6 5 +', 11", "'6 7 + 3 *', 39",
+            "'8',8", "'15 7 1 1 + - / 3 * 2 1 1 + + -', 5"})
+    void calculate_ShouldReturnCorrectDoubleAnswer(String value, double result) throws CalculateException {
+        assertThat(ReversePolishNotation.calculate(value)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2 3 ^ 2 / 6", "6 5 + -"})
+    void calculate_ShouldThrowCalculateException_ThenCalculatedValueIsInvalid(String value) throws CalculateException {
+        Throwable exception = assertThrows(CalculateException.class,
+                () -> { ReversePolishNotation.calculate(value);});
+
+        assertThat(exception).hasMessage("Invalid calculate value.");
     }
 }
